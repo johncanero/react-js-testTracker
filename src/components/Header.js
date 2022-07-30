7bc93469e5cb122fc27bc299323cdd7f4d6559b3
John Canero <johncanero.official@gmail.com>
2022-07-30 23:49:38 +0800

    Index.css Styles, Add Button and Modify Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('Click')
+  }
+
   return (
-    <header>
+    <header className='header'>
         <h1>{title}</h1>
+        <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
 }
@@ -16,5 +22,11 @@ Header.defaultProps = {
 Header.propTypes = {
     title: PropTypes.string.isRequired
 }
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor: 'black',
+// }
 
 export default Header
