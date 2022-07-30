8f68fa364973f991c91de4d7fe3dd61beb9ebd16
John Canero <johncanero.official@gmail.com>
2022-07-30 17:50:15 +0800

    App.js localhost Content and Header Title

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-function App() {
-  const name = 'John'
-  const x = true
+import Header from './components/Header'
 
+function App() {
   return (
     <div className="container">
-        <h1>Hello From React</h1>
-        <h2>Hello {name}!</h2>
-        <h2>Hello {2 + 2}!</h2>
-        {/* ? = then and : = else*/}
-        <h2>Hello {x ? 'Yes' : 'No'}!</h2>    
+        <Header />
     </div>
 
   );
 }
 
+
 export default App;
+
+
+
+
+
+
+
+
+
 
 
 // Notes: 
 // You could use <> </> instead of div
 
+    // const name = 'John'
+    // const x = true
+
+    // <div className="container">
+    //     <h1>Hello From React</h1>
+    //     <h2>Hello {name}!</h2>
+    //     <h2>Hello {2 + 2}!</h2>
+    //     {/* ? = then and : = else*/}
+    //     <h2>Hello {x ? 'Yes' : 'No'}!</h2>    
+    // </div>
