c0f043f9e0dff5e686569dad9ab8ed2080cb507a
John Canero <johncanero.official@gmail.com>
2022-07-31 23:08:41 +0800

    Update and Modify Comments and Spacing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Tasks from './components/Tasks'
@@ -11,7 +12,9 @@ import TaskDetails from './components/TaskDetails'
 
 function App() {
 
+  // Header
   const [showAddTask, setShowAddTask] = useState(false)
+  
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
@@ -97,31 +100,38 @@ function App() {
   return (
     <Router>
       <div className='container'>
+
         <Header
           onAdd={() => setShowAddTask(!showAddTask)}
           showAdd={showAddTask}
         />
+        
+
         <Routes>
-          <Route
-            path='/'
-            element={
-              <>
-                {showAddTask && <AddTask onAdd={addTask} />}
-                {tasks.length > 0 ? (
-                  <Tasks
-                    tasks={tasks}
-                    onDelete={deleteTask}
-                    onToggle={toggleReminder}
-                  />
-                ) : (
-                  'No Tasks To Show'
-                )}
-              </>
-            }
-          />
-          <Route path='/about' element={<About />} />
-          <Route path='/task/:id' element={<TaskDetails />} />
+            <Route
+              path='/'
+              element={
+                <>
+                    
+                    {showAddTask && <AddTask onAdd={addTask} />}
+
+                    {tasks.length > 0 ? (
+                      <Tasks
+                        tasks={tasks}
+                        onDelete={deleteTask}
+                        onToggle={toggleReminder}
+                      />
+                    ) : (
+                      'No Tasks To Show'
+                    )}
+                </>
+              }
+            />
+            
+            <Route path='/about' element={<About />} />
+            <Route path='/task/:id' element={<TaskDetails />} />
         </Routes>
+
         <Footer />
       </div>
     </Router>
